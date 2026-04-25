4b0fd51cb54002bd5a09f8efc4503da3067a4700
Chris Boulton <chris@chrisboulton.com>
2026-04-24 18:51:47 -0700

    add streaming tool call support

--- src/protocol.rs
+++ src/protocol.rs
@@ -39,6 +39,7 @@ pub enum SeqCommand {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
 #[serde(tag = "event")]
+#[non_exhaustive]
 pub enum MSEvent {
     SeqOpened {
         seq_id: String,
@@ -70,6 +71,30 @@ pub enum MSEvent {
         seq_id: String,
         cid: String,
         tool_calls: Vec<SeqToolCall>,
+    },
+    SeqToolCallStart {
+        seq_id: String,
+        cid: String,
+        index: u32,
+        name: String,
+        #[serde(default, skip_serializing_if = "Option::is_none")]
+        id: Option<String>,
+    },
+    SeqToolCallArgsChunk {
+        seq_id: String,
+        cid: String,
+        index: u32,
+        fragment: String,
+        #[serde(default, skip_serializing_if = "Option::is_none")]
+        id: Option<String>,
+    },
+    SeqToolCallAborted {
+        seq_id: String,
+        cid: String,
+        index: u32,
+        reason: String,
+        #[serde(default, skip_serializing_if = "Option::is_none")]
+        id: Option<String>,
     },
     SeqState {
         seq_id: String,
@@ -103,6 +128,9 @@ impl MSEvent {
             MSEvent::SeqForkFinish { cid, .. } => Some(cid.as_str()),
             MSEvent::SeqText { cid, .. } => Some(cid.as_str()),
             MSEvent::SeqToolCall { cid, .. } => Some(cid.as_str()),
+            MSEvent::SeqToolCallStart { cid, .. } => Some(cid.as_str()),
+            MSEvent::SeqToolCallArgsChunk { cid, .. } => Some(cid.as_str()),
+            MSEvent::SeqToolCallAborted { cid, .. } => Some(cid.as_str()),
             MSEvent::SeqClosed { cid, .. } => cid.as_ref().map(|s| s.as_str()),
             MSEvent::Error { cid, .. } => cid.as_ref().map(|s| s.as_str()),
             _ => None,
@@ -117,6 +145,9 @@ impl MSEvent {
             MSEvent::SeqForkFinish { .. } => "seq_fork_finish",
             MSEvent::SeqText { .. } => "seq_text",
             MSEvent::SeqToolCall { .. } => "seq_tool_call",
+            MSEvent::SeqToolCallStart { .. } => "seq_tool_call_start",
+            MSEvent::SeqToolCallArgsChunk { .. } => "seq_tool_call_args_chunk",
+            MSEvent::SeqToolCallAborted { .. } => "seq_tool_call_aborted",
             MSEvent::SeqClosed { .. } => "seq_closed",
             MSEvent::SeqState { .. } => "seq_state",
             MSEvent::Error { .. } => "error",
@@ -131,6 +162,9 @@ impl MSEvent {
             MSEvent::SeqForkFinish { seq_id, .. } => Some(seq_id.as_str()),
             MSEvent::SeqText { seq_id, .. } => Some(seq_id.as_str()),
             MSEvent::SeqToolCall { seq_id, .. } => Some(seq_id.as_str()),
+            MSEvent::SeqToolCallStart { seq_id, .. } => Some(seq_id.as_str()),
+            MSEvent::SeqToolCallArgsChunk { seq_id, .. } => Some(seq_id.as_str()),
+            MSEvent::SeqToolCallAborted { seq_id, .. } => Some(seq_id.as_str()),
             MSEvent::SeqState { seq_id, .. } => Some(seq_id.as_str()),
             MSEvent::SeqClosed { seq_id, .. } => Some(seq_id.as_str()),
             MSEvent::Error { seq_id, .. } => seq_id.as_ref().map(|s| s.as_str()),
@@ -263,6 +297,10 @@ pub struct SeqToolCall {
     pub id: Option<String>,
     pub name: String,
     pub args: String,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub index: Option<u32>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub id: Option<String>,
 }
 
 #[cfg(test)]
